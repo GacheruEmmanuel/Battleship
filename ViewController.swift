fc93e5131ee40f2c73b1307918fe670ef799176a
Papitchaya Deehamhae <200018@s020rmc148-05.local>
2018-12-11 14:17:48 -0600

    side

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Battleship
+//
+//  Created by Gacheru, Emmanuel K on 12/7/18.
+//  Copyright © 2018 Gacheru, Emmanuel K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
+
