99c88b4d51ed644b21c2c6267df5105dd7ca2a65
Johnson, Joshua A <163660@s020rmc148-04.local>
2018-12-19 13:34:15 -0600

    Started placing the ships

--- Battleship/ViewController.swift
+++ Battleship/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    
+    //2 Vars that keep track of ship placement
+    let numShipSpaces : [Int] = [5,4,3,3,2]
+    var numTilesPlacedOnShip : Int = 0
+    var whichShipOn : Int = 0
+    var directionOfShip : String = ""
 
     @IBOutlet var boardGridButtons: [UIButton]!
     var gridArr : [Position]! = [Position]()
     
-    @IBAction func changeNames(_ sender: UIButton) {
+    @IBAction func placeShip(_ sender: UIButton) {
         //sender.setTitle("ship", for: .normal)
         
         //Identifying the index of the tile pressed
@@ -26,12 +32,27 @@ class ViewController: UIViewController {
             index += 1
         }
         
-        
-        //Changing the color of the tile that was hit
-        boardGridButtons[index+1].backgroundColor = .brown
-        boardGridButtons[index-1].backgroundColor = .brown
-        boardGridButtons[index+10].backgroundColor = .brown
-        boardGridButtons[index-10].backgroundColor = .brown
+        //If no tiles have been placed, put it anywhere
+        //If one tile has been placed, set the direction after the second placement
+        //If two or more tiles have been placed, make sure that the tiles are connected and in the right direction
+        if numTilesPlacedOnShip == 0 {
+            boardGridButtons[index].backgroundColor = .brown
+            numTilesPlacedOnShip += 1
+        } else if numTilesPlacedOnShip == 1{
+            if boardGridButtons[index+1].backgroundColor == .brown || boardGridButtons[index-1].backgroundColor == .brown{
+                boardGridButtons[index].backgroundColor = .brown
+                directionOfShip = "Vertical"
+                numTilesPlacedOnShip += 1
+            } else if boardGridButtons[index+10].backgroundColor == .brown || boardGridButtons[index-10].backgroundColor == .brown {
+                boardGridButtons[index].backgroundColor = .brown
+                directionOfShip = "Horizontal"
+                numTilesPlacedOnShip += 1
+            }
+            
+            print(directionOfShip)
+        } else {
+            print("Error")
+        }
         
     }
     override func viewDidLoad() {
@@ -45,12 +66,19 @@ class ViewController: UIViewController {
         
         //For every button on the screen
         for button in boardGridButtons {
-            button.addTarget(self, action: #selector(changeNames(_:)), for: .touchUpInside)
+            button.addTarget(self, action: #selector(placeShip(_:)), for: .touchUpInside)
             button.setTitle("", for: .normal)
             button.backgroundColor = .blue
         }
+        //placeShips()
         
     }
+    
+    //the Loop for placing Ships
+    //Start with the 5 and place in decending order
+    /*func placeShips () {
+        
+    }*/
 
     
     override func didReceiveMemoryWarning() {
